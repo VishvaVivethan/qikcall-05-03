8ca1ccd896d59fb89848b4f94c2ec19da4960479
HariKrish1122001 <harikrishnannexusdigitalia@gmail.com>
2025-03-27 12:43:29 +0530

    newcode

--- backend/schema/notification.js
+++ backend/schema/notification.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports.notificationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'registeruser', 
+    required: true 
+  },
   title: { type: String, required: true },
   body: { type: String, required: true },
   date: { type: Date, default: Date.now },
